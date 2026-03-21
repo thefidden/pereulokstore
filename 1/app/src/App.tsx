845a339beaca68c07eb5a030ae1c644a04162e15
thefidden <thefidden@vk.com>
2026-03-21 23:26:47 +0500

    21.03.2026

---
+++ 1/app/src/App.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import StorePage from "./pages/StorePage.tsx";
+import ProductPage from "./pages/ProductPage.tsx";
+import CartPage from "./pages/CartPage.tsx";
+import OrdersPage from "./pages/OrdersPage.tsx";
+import OrderCheckPaymentPage from "./pages/OrderCheckPaymentPage.tsx";
+
+const Root = styled.div`
+    max-width: 1440px;
+
+    padding: 40px 0;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+    gap: 80px;
+
+    font-family: 'Jost', serif;
+    background-color: var(--color-cyan);
+`
+
+export default function App() {
+    return (
+        <Router>
+            <Root>
+                <Routes>
+                    <Route path="/" element={<Navigate to="store?type=suit" replace/>}/>
+                    <Route path="/store" element={<StorePage/>}/>
+                    <Route path="/product/:productUUID" element={<ProductPage/>}/>
+                    <Route path="/cart" element={<CartPage/>}/>
+                    <Route path="/orders" element={<OrdersPage/>}/>
+                    <Route path="/orders/:orderUUID/payment/check" element={<OrderCheckPaymentPage/>}/>
+                </Routes>
+            </Root>
+        </Router>
+    )
+}
+
---
+++ 1/app/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Root = styled.div`
+    
+`
+
+export default function Header() {
+    return (
+        <Root>
+
+        </Root>
+    )
+}
