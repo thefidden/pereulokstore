f94569a12da262f470d1f593620bb9df56a20c0e
thefidden <thefidden@vk.com>
2026-05-05 00:25:21 +0300

    05.05.2026 all files deletion

--- server/app/api/migrations/0012_product_creation_date.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("api", "0011_remove_product_creation_date"),
-    ]
-
-    operations = []
