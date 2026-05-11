98e66b9ee301d4e0fc11b750c6e8f3a9d9e64c41
thefidden <thefidden@vk.com>
2026-05-11 17:03:49 +0300

    Удаление всех файлов

--- client-vue/app/src/interfaces/CartItemInterface.ts
+++
@@ -1,7 +0,0 @@
-import type { Product } from "./ProductInterface.ts";
-
-export interface CartItem {
-    id: string,
-    amount: number,
-    product: Product
-}
--- server/app/api/migrations/0013_alter_product_options.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("api", "0012_product_creation_date"),
-    ]
-
-    operations = []
