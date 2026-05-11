d9577fc48dffe714a9a9c1fb2494811a6289d33c
thefidden <thefidden@vk.com>
2026-05-11 17:06:38 +0300

    Удаление всех файлов

--- client-react-typescript/app/src/conf.ts
+++
@@ -1,3 +0,0 @@
-export const HOST: string = `https://${window.location.hostname}`
-export const API: string = `${HOST}/api`
-export const MEDIA: string = `${HOST}/media/static`
--- server/app/api/migrations/0005_order_datetime.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("api", "0004_order_datetime_alter_order_status_and_more"),
-    ]
-
-    operations = []
