23166b6f0577dcf58ebda07f9bb31fe43e880e08
thefidden <thefidden@vk.com>
2026-05-11 19:43:34 +0300

    Удаление всех файлов

--- server/app/api/migrations/0011_remove_product_creation_date.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("api", "0010_remove_product_created_at"),
-    ]
-
-    operations = []
--- server/app/api/migrations/0019_alter_productreport_id_alter_producttag_id_and_more.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ('api', '0018_alter_producttag_id_alter_tag_id_productreport'),
-    ]
-
-    operations = []
