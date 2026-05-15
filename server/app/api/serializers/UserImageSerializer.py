6ebcf0585ff2e8d1bc0c7912f7ba46aa93c3fd4d
thefidden <thefidden@vk.com>
2026-05-15 15:10:29 +0300

    Удаление всех файлов

--- server/app/api/serializers/UserImageSerializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from api.models.UserImage import UserImage
-
-
-class UserImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserImage
-        fields = ['user', 'image']
