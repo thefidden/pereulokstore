432d447f33f695f6ba374495b2b09b68498eafb3
thefidden <thefidden@vk.com>
2026-05-15 15:17:17 +0300

    Удаление всех файлов

--- client-vue/app/src/interfaces/ProductInterface.ts
+++
@@ -1,8 +0,0 @@
-export interface Product {
-    id: string,
-    name: string,
-    type: string,
-    description: string,
-    price: number,
-    images: Array<string>
-}
--- server/app/api/serializers/AuthenticationRequestSerializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from api.models.AuthenticationRequest import AuthenticationRequest
-
-
-class AuthenticationRequestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AuthenticationRequest
-        fields = ['token', 'telegram_id', 'telegram_username', 'telegram_name', 'telegram_image']
